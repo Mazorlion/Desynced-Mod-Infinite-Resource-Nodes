792441558fd20b7f787a549c569d06c914d7f537
Maz <maz@mazorlion.com>
2023-08-16 10:42:30 -0400

    Initial Commit.

---
+++ main.lua
@@ -0,0 +1,24 @@
+local package = ...
+
+-- called when mod is initializing
+function package:init()
+    -- Find all resource node land features.
+    for k, feature in pairs(data.land_features) do
+        if feature.frame
+            and string.find(feature.frame, "resourcenode")
+            -- Don't include silica trees so they can be removed.
+            and not string.find(feature.frame, "tree")
+            and feature.nodes then
+            for k2, node in pairs(feature.nodes) do
+                if node.resource then
+                    for k3, resource in pairs(node.resource) do
+                        -- min
+                        resource[1] = REG_INFINITE
+                        -- max
+                        resource[2] = REG_INFINITE
+                    end
+                end
+            end
+        end
+    end
+end
